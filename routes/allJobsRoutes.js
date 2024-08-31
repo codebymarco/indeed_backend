2d9e8b8c6cf0834abf65e62b0520c9accc29a1fc
RamcharanMarco <miguelmarcoramcharan@gmail.com>
2024-08-31 18:38:17 +0200

    added

--- routes/allJobsRoutes.js
+++ routes/allJobsRoutes.js
@@ -7,6 +7,11 @@ router.get('/', getAllJobs)
 
 router.get('/search', searchJobs)
 
+// todo
+// put the sreahc query in the same one as the /
+// because if u use http://localhost:5000/api/jobs/search?location=all&query=all or http://localhost:5000/api/jobs?page=1
+// it gives the same result
+
 
 
 module.exports = router
